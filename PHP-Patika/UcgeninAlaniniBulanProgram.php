8d85aa6e71b67a0607ecd37b8b368227f020291e
Yiğit ÇEVİK <146030869+cvkyigit@users.noreply.github.com>
2024-05-11 19:33:25 +0300

    UcgeninAlaniniBulanProgram

---
+++ PHP-Patika/UcgeninAlaniniBulanProgram.php
@@ -0,0 +1,13 @@
+<?php
+
+/* Üçgenin alanını bulan program */
+/* Üçgen Alanı = (Taban * Yükseklik) / 2 */
+
+$taban = 20;
+$yukseklik = 5;
+
+$alan = ($taban * $yukseklik) / 2;
+
+echo "Üçgenin Alanı = " ." ". $alan;
+
+?>
