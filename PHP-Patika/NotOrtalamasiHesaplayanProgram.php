f78dde0dd9caed0cb61a1735f2d7e7facef7055b
Yiğit ÇEVİK <146030869+cvkyigit@users.noreply.github.com>
2024-05-11 11:27:03 +0300

    NotOrtalamasiHesaplayanProgram

---
+++ PHP-Patika/NotOrtalamasiHesaplayanProgram.php
@@ -0,0 +1,14 @@
+<?php
+
+/* Girilen sınav notlarının ortalamasını bulup ekrana yazdıran program */
+
+$sinav1 = 60;
+$sinav2 = 80;
+
+$ortalama = ($sinav1 + $sinav2) / 2;
+
+echo "1.Sınav Sonucunuz = " .$sinav1. "<br>";
+echo "2.Sınav Sonucunuz = " .$sinav2. "<br>";
+echo "Ortalamanız = " .$ortalama;
+
+?>
