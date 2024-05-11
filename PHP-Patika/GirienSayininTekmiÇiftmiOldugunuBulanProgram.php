02eae2d49191e673c208366fa2d3464d136127b4
Yiğit ÇEVİK <146030869+cvkyigit@users.noreply.github.com>
2024-05-11 11:29:52 +0300

    GirienSayininTekmiÇiftmiOldugunuBulanProgram

---
+++ PHP-Patika/GirienSayininTekmiÇiftmiOldugunuBulanProgram.php
@@ -0,0 +1,16 @@
+<?php
+
+/* Kullanıcıdan alınan sayını tek mi çift mi olduğunu bulan program */
+
+$sayi = 8;
+
+if ($sayi % 2 == 0)
+{
+    echo "Girilen Sayı Çifttir.";
+}
+else
+{
+    echo "Girilen Sayı Tektir.";
+}
+
+?>
