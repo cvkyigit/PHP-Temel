3411bf83c0443032dc57a9ad3826760524c18e9f
Yiğit ÇEVİK <146030869+cvkyigit@users.noreply.github.com>
2024-05-12 21:40:56 +0300

    DizilerleHaftaninGunleriniYazdiranProgram

---
+++ PHP-Patika/DizilerleHaftaninGunleriniYazdiranProgram.php
@@ -0,0 +1,13 @@
+<?php
+
+$gun = array("Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi" , "Pazar");
+
+echo $gun[0]. "<br>";
+echo $gun[1]. "<br>";
+echo $gun[2]. "<br>";
+echo $gun[3]. "<br>";
+echo $gun[4]. "<br>";
+echo $gun[5]. "<br>";
+echo $gun[6]. "<br>";
+
+?>
