6e47a0803b695b32e649ee90639187bcce072975
Yiğit ÇEVİK <146030869+cvkyigit@users.noreply.github.com>
2024-05-16 11:28:22 +0300

    DortIslemYapanProgram

---
+++ PHP-BTK Akademi/DortİslemYapanProgram.php
@@ -0,0 +1,18 @@
+<?php
+
+/* 2 sayı ile 4 işlem yapabilen (toplama, çıkarma, bölme, çarpma) programı */
+
+$sayi1 = 20;
+$sayi2 = 15;
+
+$toplama = $sayi1 + $sayi2;
+$cikarma = $sayi1 - $sayi2;
+$carpma = $sayi1 * $sayi2;
+$bolme = $sayi1 / $sayi2;
+
+echo "Toplama = " .$toplama. "<br>";
+echo "Çıkarma = " .$cikarma. "<br>";
+echo "Çarpma = " .$carpma. "<br>";
+echo "Bölme = " .$bolme;
+
+?>
